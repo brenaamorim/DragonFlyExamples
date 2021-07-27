0c0bbcde24d3a50bf8b289d5ac13f774364313fd
Brena Amorim <brena.amorim@live.com>
2021-07-27 10:52:03 -0300

    add functions for test DragonFly framework

--- DragonFlyExample/Controller/ViewController.swift
+++ DragonFlyExample/Controller/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import DragonFly
 
 class ViewController: UIViewController {
 
---
+++ DragonFlyExample/View/ExamplesView.swift
@@ -0,0 +1,20 @@
+//
+//  ExamplesView.swift
+//  DragonFlyExample
+//
+//  Created by Brena Amorim on 27/07/21.
+//
+
+import UIKit
+
+class ExamplesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
