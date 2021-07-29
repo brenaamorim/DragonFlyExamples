15bdff29b145fee7f235b8d03f30c80d0474e62c
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2021-07-29 20:31:48 -0300

    Applying the framework in font view controller.

--- DragonFlyExample/Controller/FontsViewController.swift
+++ DragonFlyExample/Controller/FontsViewController.swift
@@ -11,27 +11,31 @@ import DragonFly
 class FontsViewController: UIViewController {
     let contentView = FontsView(frame: UIScreen.main.bounds)
 
+    let font = UIFont(name: "DancingScript-Regular", size: 2)
+    lazy var fontCustom = Font(font: font)!
+    let fontSystem = Font(font: .systemFont(ofSize: 0))!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setFont()
     }
-    
+
     override func loadView() {
         super.loadView()
         self.view = contentView
     }
     
     private func setFont() {
-        contentView.large.font = .systemFont(ofSize: 34)
-        contentView.title1.font = .systemFont(ofSize: 28)
-        contentView.title2.font = .systemFont(ofSize: 22)
-        contentView.title3.font = .systemFont(ofSize: 20)
-        contentView.headline.font = .systemFont(ofSize: 17)
-        contentView.body.font = .systemFont(ofSize: 17)
-        contentView.callout.font = .systemFont(ofSize: 16)
-        contentView.subhead.font = .systemFont(ofSize: 15)
-        contentView.footnote.font = .systemFont(ofSize: 13)
-        contentView.caption1.font = .systemFont(ofSize: 12)
-        contentView.caption2.font = .systemFont(ofSize: 11)
+        contentView.large.font = fontCustom.large()
+        contentView.title1.font = fontSystem.title1()
+        contentView.title2.font = fontSystem.title2()
+        contentView.title3.font = fontSystem.title3()
+        contentView.headline.font = fontSystem.headline()
+        contentView.body.font = fontSystem.body()
+        contentView.callout.font = fontSystem.callout()
+        contentView.subhead.font = fontSystem.subhead()
+        contentView.footnote.font = fontSystem.footnote()
+        contentView.caption1.font = fontSystem.caption1()
+        contentView.caption2.font = fontSystem.caption2()
     }
 }
